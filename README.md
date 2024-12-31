98e47ecea2fa9e042bf7c600f22ff65107a5351d
bzlis <bzlis@github.com>
2024-12-31 15:13:01 -0500

    README details

---
+++ README.md
@@ -0,0 +1,10 @@
+### About
+
+Creates a typing animation for a code snippet. Uses syntax highlighting and bracket closing, as if one were typing in an IDE.
+
+### Setup
+
+```
+npm i
+npm run serve
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import { MunegascuContainer } from "./munegascu-container";
 
-const text = `this.init = (monaco.languages.getLanguages().find((l) => l.id === languageId) as unknown as any)
-.loader()
-.then(({ conf }: { conf: monaco.languages.LanguageConfiguration }) => {
-  monaco.languages.setLanguageConfiguration(languageId, conf);
-});`;
-
+const text = `const x = (...params: any[]) => {
+  foo(params);
+}`;
 const App: React.FC = () => {
   return <MunegascuContainer text={text} languageId="typescript" />;
 };
